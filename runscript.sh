6cf2408f2826ac01716813ae5ea8913688adb8cb
mridul2101 <mridul2101@gmail.com>
2015-02-07 13:32:38 +0530

    git update

--- runscript.sh
+++ runscript.sh
@@ -1,6 +1,7 @@
 git status
 git add -A
 git commit -m "git update"
-heroku config:set NODE_ENV=production
-git push heroku master
-heroku ps:scale web=1heroku open
+#heroku config:set NODE_ENV=production
+git push heroku
+#heroku ps:scale web=1
+heroku open
